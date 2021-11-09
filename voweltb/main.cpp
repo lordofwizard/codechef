723d71f7e96c4d7c0fdfefe6a8e0e4b56a6ca22b
lordofwizard <advaitpandharpurkar619@gmail.com>
2021-11-09 16:39:40 +0530

    completed this used switch cases.. very easy problem

---
+++ voweltb/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char c;
+	cin >> c;
+	switch(c){
+		case 'A':
+			cout << "Vowel" << endl;
+			break;
+		case 'E':
+			cout << "Vowel" << endl;
+			break;
+		case 'I':
+			cout << "Vowel" << endl;
+			break;
+		case 'O':
+			cout << "Vowel" << endl;
+			break;
+		case 'U':
+			cout << "Vowel" << endl;
+			break;
+		default:
+			cout << "Consonant" << endl;
+	}
+	return 0;
+}
