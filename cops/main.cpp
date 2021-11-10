5075c02be7c350ae780429f02f2d486311d9dc0a
lordofwizard <advaitpandharpurkar619@gmail.com>
2021-11-10 18:04:33 +0530

    well wrote some code about cops... interesting problem and gonna solve it but not now

---
+++ cops/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorighm>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int num;
+		cin >> num;
+		int max_speed;
+		cin >> max_speed;
+		int max_time;
+		cin >> max_time;
+		int house[num];
+		for(int i =0;i < num; i++){
+			cin >> house[i];
+		}
+		// REAL LOGIC
+		int distance = max_speed * max_time;
+		
+		std::vector<int> cops_see_house;
+
+		for(int z=0;z<num;z++){
+			for (int x=0;
+
+		}
+	
+	}
+
+
+	return 0;
+}
