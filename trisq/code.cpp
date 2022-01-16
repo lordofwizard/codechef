dc6b1bb614b8417dcfba7eb7e59676e0729a8fd7
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-01-16 10:21:24 +0530

    solved this one with a little help from geeksforgeeks

---
+++ trisq/code.cpp
@@ -0,0 +1,22 @@
+/* This project template is created by LordOfWizard
+ * https://github.com/lordofwizard
+ * YouTube https://0x0.st/NUMD
+ */
+
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int b;
+		cin >> b;
+		b -=2;
+		b = floor(b/2);
+		cout << b * (b+1)/2 << endl;
+		
+	}
+}
