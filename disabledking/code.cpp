6a0ada08ad61bde317bc45b11b1f51a15a698ea1
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-01-17 16:55:28 +0530

    solved supchef in less than minute without any help i am godly good

--- disabledking/code.cpp
+++ disabledking/code.cpp
@@ -14,9 +14,6 @@ int main(){
 	int t;
 	cin >> t;
 	while(t--){
-		int m , n , k;
-		cin >> m >> n >> k;
-		if ( (m - ( n * k )) > 0 ) cout << "YES" << endl;
-		else cout << "NO" << endl;		
+		
 	}
 }
