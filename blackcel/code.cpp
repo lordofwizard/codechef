907851936d0452cde6960720523a3fb998089199
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-01-18 13:08:19 +0530

    soo easy

---
+++ blackcel/code.cpp
@@ -0,0 +1,17 @@
+/* This project template is created by LordOfWizard
+ * https://github.com/lordofwizard
+ * YouTube https://0x0.st/NUMD
+ */
+#define vi vector<int>
+#define vc vector<char>
+
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	cout << (int)(t*t)/2 << endl;
+}
