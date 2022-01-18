0b657b54757dd4c7f353edc1a32f5a7475bbd3ab
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-01-18 11:12:53 +0530

    solved this problem easy af

--- csum/code.cpp
+++ csum/code.cpp
@@ -2,37 +2,31 @@
  * https://github.com/lordofwizard
  * YouTube https://0x0.st/NUMD
  */
-#define vi vector<int>
-#define vc vector<char>
 
 #include <bits/stdc++.h>
-#include <vector>
 
 using namespace std;
 
-int main(){
-	int t;
+signed main(){
+	long long t;
 	cin >> t;
 	while(t--){
-		unsigned int n,k,j;
-		cin >> n >> k;
-		vector<int> list[n];
-		bool found = false;
-		for(int i = 0 ; i < n ; i++){
+		   bool found = false;
+		   long long n , k;
+		   long long list[n];
+		   cin >> n >> k;
+		for(long long i = 0; i<n; i++){
 			cin >> list[i];
-		}	
-/*		for(auto & x : list){
-			for( auto & y : list ) {
-				   if ( (k - x == y) && (x != y)) found = true;
 		}
+		sort(list , list+n);
+		long long i = 0; long long j = n-1;
+		while(i<j){
+			if (list[i] + list[j] == k) {found = true;break;}
+			if (list[i] + list[j] < k) ++i;
+			if (list[i] + list[j] > k) --j;
+		}
+	if (found) cout << "Yes" << endl;
+	else cout << "No" << endl;
 	}
-*/
-
-		for (int i = 0; i < n; i++){
-			for (j = 0; j<n ; j++){
-				if ( ( k - list[i] == list[j]) && (list[i] != list[j]) && (i != j) && (!(i==j))) found = true;
-		}
-		}
-		if (found) cout << "Yes" << endl;
-		else cout << "No" << endl;
-}}
+	return 0;
+}
