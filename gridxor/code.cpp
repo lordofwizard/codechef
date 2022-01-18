3d31521da1d1a8a3ee8518e047e4c01c3be93abe
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-01-18 12:33:19 +0530

    solved this one but fucked up the first test case

---
+++ gridxor/code.cpp
@@ -0,0 +1,19 @@
+/* This project template is created by LordOfWizard
+ * https://github.com/lordofwizard
+ * YouTube https://0x0.st/NUMD
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+		int t;
+		cin >> t;
+		while(t--){
+		int n=1;
+		int x;
+		cin >> x;
+		for(int j = 1; j <= x; j++){
+				for(int i = 1; i <= x; i++)cout << n << " ";
+			cout << endl;
+		}}
+		return 0;
+}
